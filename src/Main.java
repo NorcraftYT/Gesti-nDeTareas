e660fe096b31df059a61331fb2d3ee8c7706a66b
NorcraftYT <dnprty@gmail.com>
2026-06-19 18:43:51 +0200

    añado funcionalidad borrado de tarea

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,31 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void cambiarEstado(ArrayList<Boolean> estados, int posicion) {
 
+    public static void cambiarEstado(ArrayList<Boolean> estados, int posicion) {
         boolean estadoActual = estados.get(posicion);
         estados.set(posicion, !estadoActual);
     }
 
-    public static void main(String[] args) {
+    public static void eliminarTarea(ArrayList<String> tareas, ArrayList<Boolean> estados, int posicion) {
+        tareas.remove(posicion);
+        estados.remove(posicion);
+    }
 
+    public static void main(String[] args) {
         ArrayList<String> tareas = new ArrayList<>();
         ArrayList<Boolean> completadas = new ArrayList<>();
 
         tareas.add("Estudiar para el examen");
-        completadas.add(false); // Empieza como pendiente (false)
+        completadas.add(false);
 
         tareas.add("Configurar el servidor");
         completadas.add(false);
 
-        System.out.println("Tarea: " + tareas.get(0) + " | ¿Completada?: " + completadas.get(0));
+        System.out.println("Lista antes de borrar: " + tareas);
 
-        cambiarEstado(completadas, 0);
+        eliminarTarea(tareas, completadas, 0);
 
-        System.out.println("Tarea: " + tareas.get(0) + " | ¿Completada?: " + completadas.get(0));
+        System.out.println("Lista después de borrar: " + tareas);
     }
 }
