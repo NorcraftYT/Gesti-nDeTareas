28a32a87e57cd3447f20c86fed5f8ab7f41c656e
NorcraftYT <dnprty@gmail.com>
2026-06-19 18:33:33 +0200

    Añado funcionalidad cambiar estado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+
 public class Main {
+    public static void cambiarEstado(ArrayList<Boolean> estados, int posicion) {
+
+        boolean estadoActual = estados.get(posicion);
+        estados.set(posicion, !estadoActual);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Bienvenido al gestor de tareas");
+
+        ArrayList<String> tareas = new ArrayList<>();
+        ArrayList<Boolean> completadas = new ArrayList<>();
+
+        tareas.add("Estudiar para el examen");
+        completadas.add(false); // Empieza como pendiente (false)
+
+        tareas.add("Configurar el servidor");
+        completadas.add(false);
+
+        System.out.println("Tarea: " + tareas.get(0) + " | ¿Completada?: " + completadas.get(0));
+
+        cambiarEstado(completadas, 0);
+
+        System.out.println("Tarea: " + tareas.get(0) + " | ¿Completada?: " + completadas.get(0));
     }
 }
-
